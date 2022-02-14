0f179c34dd59820eb794820fbc4940f2d94e47eb
RabbiaKhalid <65655507+RabbiaKhalid@users.noreply.github.com>
2022-02-14 10:09:39 +0500

    React Test Project

---
+++ src/TestComponent.js
@@ -0,0 +1,12 @@
+
+function TestComponent(){
+     return
+     (
+        <div>
+            <h2> Hello World</h2>
+            <h3> Test Component</h3>
+        </div>
+
+     );
+ }
+ export default TestComponent;
